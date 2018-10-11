d2f32e3456bfc8bf0fa4d7a8487ade3178ba7a6d
jg <jgi@jgwill.com>
2018-10-11 09:53:07 -0400

    init the NodeEclipse
    plus documented how to install

---
+++ 02_create_project.sh
@@ -0,0 +1,14 @@
+
+#create necessary .project file to import into Eclipse
+nodeclipse -p
+
+# In Eclipse/Enide select File -> Import... -> General / Existing Projects into Workspace
+#  and enter project directory: /Users/caroline/Dropbox/nodeJG/x/x1810110922__Eclipse_N_NodeJS
+
+MANUAL ACTION {
+
+    DrapNDrop INSTALL 
+        from : http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=2566102
+        to EclipseIDE, follow install Enide (Studio) 2015 - Node.js, JavaScript, Java and Web Tools by clicking the Install button
+        
+}
